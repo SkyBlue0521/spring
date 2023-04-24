56cc36c47b69c16341d7e31fd0e5d08c36a623f8
장다희 <memory5112@naver.com>
2023-04-24 11:29:53 +0900

    Spring-pro03

---
+++ pro03/src/main/java/com/spring/ex01/MembedrServlet.java
@@ -0,0 +1,35 @@
+package com.spring.ex01;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/mem.do")
+public class MembedrServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request,response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html:charset=ut-8");
+		MemberDAO dao = new MemberDAO();
+		List<MemberVO> membersList = dao.selectAllMemberList();
+		//List<HashMap<String, String>>membersList =
+		dao.selectAllMemberList();
+			request.setAttribute("membersList", membersList);
+	}
+}
